1732a5ccccfdfdfc3129a35265e18252e9d0a79c
Jordan <jordan@chude.co>
2023-07-10 22:09:02 -0400

    Refactored code to include a dictionary for columns_to_include and added code to convert columns to numeric values and handle NaN values

--- notebooks/linear_regression.py
+++ notebooks/linear_regression.py
@@ -15,7 +15,23 @@ dtype_mapping = {
     'price': 'float'
 }
 
-columns_to_include = ['bed', 'bath', 'acre_lot', 'house_size', 'price']
+# columns_to_include = ['bed', 'bath', 'acre_lot', 'house_size', 'price']
+
+columns_to_include = {
+    'bed': 'bed',
+    'bath': 'bath',
+    'acre_lot': 'acre_lot',
+    'house_size': 'house_size',
+    'price': 'price'
+}
+
+housing_data = pd.read_csv(filepath, dtype=dtype_mapping, usecols=list(columns_to_include.values())).dropna()
+
+columns_to_include['bed'] = pd.to_numeric(columns_to_include['bed'], errors='coerce')
+columns_to_include['bath'] = pd.to_numeric(columns_to_include['bath'], errors='coerce')
+columns_to_include['acre_lot'] = pd.to_numeric(columns_to_include['acre_lot'], errors='coerce')
+columns_to_include['house_size'] = pd.to_numeric(columns_to_include['house_size'], errors='coerce')
+columns_to_include['price'] = pd.to_numeric(columns_to_include['price'], errors='coerce')
 
 housing_data = pd.read_csv(filepath, dtype=dtype_mapping, usecols=columns_to_include).dropna()
 
@@ -23,17 +39,6 @@ print(housing_data.dtypes)
 
 x = housing_data[['bed', 'bath', 'acre_lot', 'house_size']]
 y = housing_data['price']
-
-# Now you can check for NaN values and proceed with your analysis
-
-# Assuming you have a DataFrame called 'data' with your input data
-na_mask = housing_data.isna()  # Create a boolean mask where True indicates NaN values
-
-# Count the number of NaN values in each column
-na_counts = na_mask.sum()
-
-# Print the columns with NaN values and their corresponding counts
-print(na_counts[na_counts > 0])
 
 # Train the model
 reg = LinearRegression()
@@ -43,8 +48,15 @@ reg.fit(x, y)
 print("Want to buy a new house and want to see how prices change as the features of the house change? I just need a little more information from you to help!")
 
 time.sleep(2)
-
-name = input("First off, what's your name?")
+name = input("First off, what's your name? ")
 print ("Okay, great to meet you " + name + " ! Let's get started!")
 
 time.sleep(2)
+bed = float(input("How many bedrooms do you want? "))
+bath = float(input("How many bathrooms do you want? "))
+acre_lot = float(input("How many acres do you want? "))
+house_size = float(input("How big do you want your house to be (in square feet)? "))
+time.sleep(2)
+
+y_pred = reg.predict(np.array([[bed, bath, acre_lot, house_size]]))
+print("Okay, based on the information you gave me, I predict that your house will cost $" + str(y_pred[0]) + " !")
